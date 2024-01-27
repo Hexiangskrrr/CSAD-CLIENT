e36633caf889156e8b2ad919a80e23677a66c5bc
Hexiang <hexiang.22@ichat.sp.edu.sg>
2024-01-27 16:54:30 +0800

    add get method for menu data

--- src/components/Food/AvailableFood.js
+++ src/components/Food/AvailableFood.js
@@ -1,56 +1,57 @@
 import FoodItem from "./FoodItem/FoodItem"
-
+import axios from "axios"
 import FoodFilterItem from "./FoodFilter/FoodFilterItem"
 import styles from './AvailableFood.module.css'
-import  {  useState  } from 'react'
-import DUMMY_FOOD from "./DUMMY_FOOD" // to be replaced with real data
-
-
-
-const selections = ["Drinks", "Dessert", "Seasonal", "Set Meal"]
-
-
-
+import { useState, useEffect } from 'react'
 
 const AvailableFood = () => {
+  const [selectedItem, setSelectedItem] = useState()
+  const [foodList, setFoodList] = useState([])
 
-    const [selectedItem, setSelectedItem] = useState();
-  
-    const getSelectionItem = (selection) => {
-      setSelectedItem(selection)
-    };
+  const selections = ["Drinks", "Dessert", "Seasonal", "Set Meal"]
 
-    const selectionList = selections.map((selection) => (
-      <FoodFilterItem
+  useEffect(() => {
+    axios.get('menu')
+      .then(response => {
+        setFoodList(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const getSelectionItem = (selection) => {
+    setSelectedItem(selection);
+  };
+
+  const selectionList = selections.map((selection) => (
+    <FoodFilterItem
       key={selection}
       selection={selection}
       isSelected={selection === selectedItem}
       onClick={() => getSelectionItem(selection)}
-      />
-    ))
-  
-
-  const filteredFood = DUMMY_FOOD.filter((food) => food.category === selectedItem);
-
-  const foodList = filteredFood.map((food) => (
-    <FoodItem
-      key = {food.id}
-      id  = {food.id}
-      name = {food.name}
-      price = {food.price}
-      description = {food.description}
-      category = {food.category}
     />
+  ));
 
-  ))
-  
-  return(
+  const filteredFood = foodList.filter((food) => food.category === selectedItem);
+
+  const foodItems = filteredFood.map((food) => (
+    <FoodItem
+      key={food.id}
+      id={food.id}
+      name={food.name}
+      price={food.price}
+      description={food.description}
+      category={food.category}
+    />
+  ));
+
+  return (
     <div>
       <div className={styles.container}>{selectionList}</div>
-      <div>{foodList}</div>
+      <div>{foodItems}</div>
     </div>
-  )
+  );
+};
 
-}
-
-export default AvailableFood
+export default AvailableFood;
