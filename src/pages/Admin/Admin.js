ea631d53367543b1ad7d982a91af4b92b9f74497
Hexiang <hexiang.22@ichat.sp.edu.sg>
2024-01-30 00:30:01 +0800

    add food form to add food from client

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import axios from "axios"
 import FoodTable from '../../components/Food/FoodTable/FoodTable'
+import FoodForm from '../../components/Food/FoodForm/FoodForm'
 
 const Admin = () => {
 
   return (
     <div>
       <FoodTable />
+      <FoodForm />
     </div>
   )
 }
