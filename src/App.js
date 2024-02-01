365c47e025e1f2286f8afaf14aca00931dfba86a
Hexiang <hexiang.22@ichat.sp.edu.sg>
2024-02-02 00:17:59 +0800

    fix merge issue for checkout and master in  app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Layout/Header/Header';
-import { CartProvider } from 'react-use-cart';
-import AvailableFood from './components/Food/AvailableFood';
-
-function App() {
-  return (
-    <CartProvider>
-      <div>
-        <div id='header'><Header/></div>
-        <div id='availablefood'><AvailableFood/></div>
-      </div>
-    </CartProvider>
-  );
-}
-
-export default App;
-
+import React from 'react'
+import './App.css'
 import {  BrowserRouter, Routes, Route  } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Menu from './pages/Menu/Menu'
+
+
 
 function App() {
 
