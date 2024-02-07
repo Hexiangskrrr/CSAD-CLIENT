7063e0e6e82c582ea0cd64cc018076ff13bd64c1
Hexiang <hexiang.22@ichat.sp.edu.sg>
2024-02-07 18:22:11 +0800

    add axios to admin auth

--- src/pages/Admin/AdminAuth.js
+++ src/pages/Admin/AdminAuth.js
@@ -1,24 +1,61 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AdminAuth = ({ onAdminAuth }) => {
   const navigate = useNavigate();
+  const SERVER_URL = "http://localhost:5003";
+
+  const [loginError, setLoginError] = useState(false);
+  const [isUsernameEmpty, setUsernameState] = useState(false);
+  const [isPasswordEmpty, setPasswordState] = useState(false);
 
   const [adminLoginData, setAdminLoginData] = useState({
     username: "",
     password: "",
   });
 
+  const validateUsername = () => {
+    if (adminLoginData.username == "") {
+      setUsernameState(true);
+      return true;
+    } else {
+      setUsernameState(false);
+      return false;
+    }
+  };
+
+  const validatePassword = () => {
+    if (adminLoginData.password == "") {
+      setPasswordState(true);
+      return true;
+    } else {
+      setPasswordState(false);
+      return false;
+    }
+  };
+
   const handleAdminLogin = (e) => {
     e.preventDefault();
-    if (
-      adminLoginData.username == "admin" &&
-      adminLoginData.password == "123123"
-    ) {
-      onAdminAuth(); // Trigger authentication
-      navigate("/adminconsole");
-    } else {
-      console.log("incorrect username or pw");
+    const isUsernameEmpty = validateUsername();
+    const isPasswordEmpty = validatePassword();
+    if (!isUsernameEmpty && !isPasswordEmpty) {
+      axios
+        .post(`${SERVER_URL}/adminlogin`, adminLoginData)
+        .then((response) => {
+          console.log("Authentication response:", response.data);
+          if (response.data.authenticated) {
+            onAdminAuth(); // Trigger authentication
+            navigate("/adminconsole");
+          } else {
+            console.error("Authentication failed");
+            setLoginError(true);
+          }
+        })
+        .catch((error) => {
+          console.error("Error during authentication", error);
+          setLoginError(true);
+        });
     }
   };
 
@@ -26,7 +63,7 @@ const AdminAuth = ({ onAdminAuth }) => {
     <div>
       <h1>Admin Login</h1>
       <form onSubmit={handleAdminLogin}>
-        Username:{" "}
+        Username:
         <input
           type="text"
           name="username"
@@ -34,8 +71,9 @@ const AdminAuth = ({ onAdminAuth }) => {
             setAdminLoginData({ ...adminLoginData, username: e.target.value })
           }
         ></input>
+        {isUsernameEmpty ? <span>Username cannot be empty</span> : null}
         <br />
-        Password:{" "}
+        Password:
         <input
           type="password"
           name="password"
@@ -43,9 +81,11 @@ const AdminAuth = ({ onAdminAuth }) => {
             setAdminLoginData({ ...adminLoginData, password: e.target.value })
           }
         ></input>
+        {isPasswordEmpty ? <span>Password cannot be empty</span> : null}
         <br />
         <input type="submit" value={"Log In"}></input>
       </form>
+      {loginError ? <p>Incorrect username or password</p> : null}
     </div>
   );
 };
